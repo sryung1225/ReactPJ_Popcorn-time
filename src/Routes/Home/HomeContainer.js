13ad2a7630f047f66b5e0b1753d12ce7977dee83
sryung1225 <sryung1225@gmail.com>
2020-05-15 19:43:43 +0900

    container

--- src/Routes/Home/HomeContainer.js
+++ src/Routes/Home/HomeContainer.js
@@ -1,5 +1,6 @@
 import React from "react";
 import HomePresenter from "./HomePresenter";
+import { movieApi } from "api";
 
 export default class extends React.Component {
     state = {
@@ -10,9 +11,38 @@ export default class extends React.Component {
         loading: true
     };
 
+    async componentDidMount(){
+        try{
+            const { 
+                data: { results: nowPlaying }
+            } = await movieApi.nowPlaying();
+            //console.log(nowPlaying);
+            const { 
+                data: { results: upcoming }
+            } = await movieApi.upcoming();
+            const { 
+                data: { results: popular }
+            } = await movieApi.popular();
+            this.setState({
+                nowPlaying,
+                upcoming,
+                popular
+            });
+        } catch{
+            this.setState({
+                error: "Can't find movies information."
+            }); /* 에러날 경우 */
+        } finally{
+            this.setState({
+                loading: false
+            }); /* 성공하던 에러나던 결과적으로는 false를 보여주도록*/
+        } 
+    }
+
     render() {
         const { nowPlaying, upcoming, popular, error, loading} = this.state;
-        return (
+        console.log(this.state);
+        return(
             <HomePresenter  
                 nowPlaying={nowPlaying}
                 upcoming={upcoming}
--- src/api.js
+++ src/api.js
@@ -3,7 +3,7 @@ import axios from "axios";
 const api = axios.create({
     baseURL: "https://api.themoviedb.org/3/",
     params: {
-        api_key: "10923b261ba94d897ac6b81148314a3f",
+        api_key: "13da49bd686c54480738331655b4c7d20",
         language: "en-US"
     }
 });
@@ -26,7 +26,7 @@ export const movieApi = {
             }
         }
     )
-}
+};
 
 export const tvApi = {
     topRated: () => api.get("tv/top_rated"),
@@ -46,4 +46,4 @@ export const tvApi = {
             }
         }
     )
-}
+};
